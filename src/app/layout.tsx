7fd4aa1a69f42489dda119aec8e4d754cd85588b
Sergei Smekalin <sergei.smekalin@b2broker.com>
2024-05-02 12:02:53 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
